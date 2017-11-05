a84240e2866f6502125ad4e64bdc26a112ea951c
Mark Schack <mschack@rogers.com>
2017-11-05 11:47:43 -0500

    added Synth Drums

--- src/mscHack.cpp
+++ src/mscHack.cpp
@@ -7,9 +7,10 @@ void init(rack::Plugin *p)
 	plugin = p;
 	plugin->slug = "mscHack";
 	plugin->name = "mscHack";
-	plugin->homepageUrl = "";
+	plugin->homepageUrl = "https://github.com/mschack/VCV-Rack-Plugins";
 
     createModel<Seq_3x16x16_Widget>(plugin, "Seq_3ch_16step", "SEQ 3 x 16 Programmable");
+    createModel<SynthDrums_Widget>(plugin, "SynthDrums", "Synth Drums");
     createModel<Mix_4x4_Stereo_Widget>(plugin, "Mix_4x4_Stereo", "MIXER 4x4 Stereo");
     createModel<Mix_4x4_Stereo2_Widget>(plugin, "Mix_4x4_Stereo(2)", "MIXER 4x4 Stereo 2 with EQ");
     createModel<Seq_Triad_Widget>(plugin, "TriadSeq", "Triad Sequencer Programmable");
